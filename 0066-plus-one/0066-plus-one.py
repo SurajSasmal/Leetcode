d94d109bc3aba263e80bf4d06d319e672f677858
Suraj Sasmal <surajsasmal9777@gmail.com>
2026-07-01 21:27:20 +0530

    Time: 0 ms (100%), Space: 19.3 MB (55.99%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -2,7 +2,6 @@ class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         i = 0
         sum = 0
-        add = 1
         n = len(digits)
         for i in range(n):
             sum = sum * 10 + digits[i]
