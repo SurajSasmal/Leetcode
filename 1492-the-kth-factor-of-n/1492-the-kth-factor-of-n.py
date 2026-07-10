c3c56e776939ee2339aff9fe1dc58c226a7f926e
Suraj Sasmal <surajsasmal9777@gmail.com>
2026-07-10 22:16:35 +0530

    Time: 0 ms (100%), Space: 19.3 MB (54.83%) - LeetHub

--- 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.py
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.py
@@ -1,18 +1,18 @@
 class Solution:
     def kthFactor(self, n: int, k: int) -> int:
-        lis = []
+        factors = []
 
         for i in range(1,n+1):
             if n % i == 0:
-                lis.append(i)
+                factors.append(i)
         
-        m = len(lis)
+        m = len(factors)
 
         # for j in range(m):
         #     if j == k -1:
         #         return lis[k-1]
 
         if m >= k:
-            return lis[k - 1]
+            return factors[k - 1]
 
         return -1
