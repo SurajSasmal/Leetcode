3c70a454ccda3018acf80eb2e058fa1682fd7889
Suraj Sasmal <surajsasmal9777@gmail.com>
2026-07-14 13:34:49 +0530

    Time: 0 ms (100%), Space: 19.3 MB (18.17%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,15 +1,19 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        i = 0
-        sum = 0
-        n = len(digits)
-        for i in range(n):
-            sum = sum * 10 + digits[i]
         
-        sum = sum + 1
-        arr = []
+        n = len(digits)
 
-        while(sum > 0):
-            arr.append(sum % 10)
-            sum = sum // 10
-        return arr[::-1]
+        nums = 0
+
+        for i in range(n):
+            nums = nums * 10 + digits[i]
+        total = nums + 1
+
+        last = []
+
+        while(total != 0):
+            rem = total % 10
+            last.append(rem)
+            total = total // 10
+
+        return last[::-1]
