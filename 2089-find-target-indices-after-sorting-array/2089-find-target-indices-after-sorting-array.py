a0d84b1468dc01dd48e347705ecabf9466653247
Suraj Sasmal <surajsasmal9777@gmail.com>
2026-07-20 09:58:08 +0530

    Time: 0 ms (100%), Space: 19.3 MB (61.41%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.py
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.py
@@ -1,13 +1,63 @@
 class Solution:
     def targetIndices(self, nums: List[int], target: int) -> List[int]:
         
+        # it is done by brute force 
+        # time complexity - 0(n log n)
+        # nums.sort()
+
+        # arr = []
+
+        # n = len(nums)
+
+        # for i in range(n):
+        #     if nums[i] == target:
+        #         arr.append(i)
+        # return arr
+
+# by binary search 
+
         nums.sort()
 
-        arr = []
+        # first occurrance
 
-        n = len(nums)
+        left = 0
+        right = len(nums) - 1
+        first = -1 
 
-        for i in range(n):
-            if nums[i] == target:
-                arr.append(i)
-        return arr
+        while left <= right:
+            mid = (left + right) // 2
+
+            if nums[mid] == target:
+                first = mid 
+                right = mid - 1       # left side search
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        if first == -1:
+            return []
+        
+        # last ocuurance
+
+        left = 0
+        right = len(nums) - 1
+        last = -1
+
+        while left <= right:
+            mid = (left + right) // 2
+            
+            if nums[mid] == target:
+                last = mid 
+                left = mid + 1
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        ans = []
+
+        for i in range(first, last+1):
+            ans.append(i)
+        
+        return ans
