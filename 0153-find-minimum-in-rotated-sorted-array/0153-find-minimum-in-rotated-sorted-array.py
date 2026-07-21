0846cd2d85b4ed57e01ecaf432c9525d5aac0ed6
Suraj Sasmal <surajsasmal9777@gmail.com>
2026-07-21 13:13:28 +0530

    Time: 0 ms (100%), Space: 19.5 MB (24.43%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -1,25 +1,40 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
         
-        smallest = float('inf')
+        # smallest = float('inf')
+        # left = 0
+
+        # right = len(nums) - 1
+
+
+        # while left <= right:
+
+        #     mid = (left + right) // 2
+
+        #     if nums[mid] < nums[left] and nums[mid] < nums[right]:
+        #         smallest = min(smallest,nums[mid])
+        #         left = left + 1
+        #         right = right - 1
+
+        #     elif nums[mid] > nums[right]:
+        #         smallest = min(smallest,nums[mid])
+        #         left = mid + 1
+        #     else:
+        #         smallest = min(smallest,nums[mid])
+        #         right = mid - 1
+        # return smallest 
+
+
+# or 
         left = 0
 
         right = len(nums) - 1
 
-
-        while left <= right:
-
+        while left < right:
             mid = (left + right) // 2
 
-            if nums[mid] < nums[left] and nums[mid] < nums[right]:
-                smallest = min(smallest,nums[mid])
-                left = left + 1
-                right = right - 1
-
-            elif nums[mid] > nums[right]:
-                smallest = min(smallest,nums[mid])
+            if nums[mid] > nums[right]:
                 left = mid + 1
             else:
-                smallest = min(smallest,nums[mid])
-                right = mid - 1
-        return smallest 
+                right = mid 
+        return nums[left]
