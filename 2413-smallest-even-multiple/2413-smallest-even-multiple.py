0781c84d46270d7f4229038e6023d51574f1bb22
Suraj Sasmal <surajsasmal9777@gmail.com>
2026-07-29 22:32:12 +0530

    Time: 0 ms (100%), Space: 19.2 MB (45.62%) - LeetHub

--- 2413-smallest-even-multiple/2413-smallest-even-multiple.py
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.py
@@ -2,9 +2,8 @@ class Solution:
     def smallestEvenMultiple(self, n: int) -> int:
         
         if n % 2 == 0:
-            div = n // 2
+            rem = n // 2
 
-            return div * 2
-        
+            return rem * 2
+            
         return n * 2
-        
