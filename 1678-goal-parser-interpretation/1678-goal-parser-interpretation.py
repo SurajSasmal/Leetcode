4a4ad506507456cad093d413d86fa631980b48b3
Suraj Sasmal <surajsasmal9777@gmail.com>
2026-07-31 17:55:23 +0530

    Time: 53 ms (7.77%), Space: 19.4 MB (9.49%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
@@ -1,4 +1,18 @@
 class Solution:
-    def interpret(self, command: str) -> str:
+    def interpret(self, s: str) -> str:
         
-        return command.replace('()','o').replace('(al)','al')
+        # return command.replace('()','o').replace('(al)','al')
+
+        d = {"(al)":"al","()":"o","G":"G"}
+
+        tmp = ""
+
+        result = ""
+
+        for i in range(len(s)):
+            tmp += s[i]
+
+            if tmp in d:
+                result += d[tmp]
+                tmp = ""
+        return result
