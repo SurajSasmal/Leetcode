8d135ff6a2ed8907b366a8660e071f3bb91049d3
Suraj Sasmal <surajsasmal9777@gmail.com>
2026-08-06 11:44:10 +0530

    Time: 0 ms (100%), Space: 19.3 MB (80.85%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
@@ -3,11 +3,31 @@ class Solution:
         
         # return list(set(nums1) & set(nums2))
 
-        set1 = set(nums1)
-        ans = set()
+        # set1 = set(nums1)
+        # ans = set()
 
-        for num in nums2:
-            if num in set1:
-                ans.add(num)
+        # for num in nums2:
+        #     if num in set1:
+        #         ans.add(num)
 
-        return list(ans)
+        # return list(ans)
+
+        nums1.sort()
+        nums2.sort()
+        n = len(nums1)
+        m = len(nums2)
+        i = 0
+        j = 0
+        ans = []
+        while i < n and j < m:
+            if nums1[i] == nums2[j]:
+
+                if not ans or ans[-1] != nums1[i]:
+                    ans.append(nums1[i])
+                i += 1
+                j += 1
+            elif nums1[i] < nums2[j]:
+                i += 1
+            else:
+                j += 1
+        return ans
