b97c201d422ac29573f1101bb1ffb1eab3fc20cb
Suraj Sasmal <surajsasmal9777@gmail.com>
2026-08-06 10:47:12 +0530

    Time: 0 ms (100%), Space: 19.3 MB (32.93%) - LeetHub

--- 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.py
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.py
@@ -12,3 +12,17 @@ class Solution:
             return m 
         else:
             return self.smallestNumber(m+1,t)
+
+        for i in range(n,n+100):
+            
+            multi = 1
+            m = i
+
+            while m > 0:
+                rem = m % 10
+                multi = multi * rem 
+                m  = m // 10
+            
+            if multi % t == 0:
+                return m
+            
