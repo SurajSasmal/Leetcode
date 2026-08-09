a9c06a140abcb36f086afc49d2db00e82fde2945
Suraj Sasmal <surajsasmal9777@gmail.com>
2026-08-10 00:31:20 +0530

    Time: 0 ms (100%), Space: 19.5 MB (9.14%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -7,19 +7,22 @@ class Solution:
         idx = m + n - 1
 
         i = m - 1
+
         j = n - 1
 
-        while(i >= 0 and j >= 0):
+        while i >= 0 and j >= 0:
             if nums1[i] >= nums2[j]:
                 nums1[idx] = nums1[i]
+
                 idx -= 1
                 i -= 1
             else:
                 nums1[idx] = nums2[j]
                 idx -= 1
                 j -= 1
-        while(j >= 0):
+        while j >= 0:
             nums1[idx] = nums2[j]
             idx -= 1
             j -= 1
-        return idx
+        
+        return  nums1
