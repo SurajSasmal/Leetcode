8d6bf6f2396d5dae2f5f3bc53df2441dfdfeb608
Suraj Sasmal <surajsasmal9777@gmail.com>
2026-08-11 00:01:52 +0530

    Time: 0 ms (100%), Space: 19.5 MB (5.7%) - LeetHub

--- 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.py
+++ 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.py
@@ -3,14 +3,22 @@ class Solution:
         
         totalCount = 0
 
-        for num in nums:
-            count = 0
+        # for num in nums:
+        #     count = 0
 
-            while num != 0:
-                rem = num % 10
-                count += 1
-                num = num // 10
-            if count % 2 == 0:
+        #     while num != 0:
+        #         rem = num % 10
+        #         count += 1
+        #         num = num // 10
+        #     if count % 2 == 0:
+        #         totalCount += 1
+        # return totalCount 
+
+        for num in nums:
+            
+            length = len(str(abs(num)))
+
+            if length % 2 == 0:
                 totalCount += 1
         return totalCount 
         
