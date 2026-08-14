f2d5d024ec818391752c2227eb21532e9c519a65
Suraj Sasmal <surajsasmal9777@gmail.com>
2026-08-14 21:30:15 +0530

    Time: 0 ms (100%), Space: 20.5 MB (60.01%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,37 +1,37 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        # this problem will divide in to two parts 
-        # first ocuurance 
-        # last ocurrance 
+        
+        # this problem will divide into two parts 
+        # first half 
+        # last half 
 
-        left = 0
-        right = len(nums) - 1
+        l = 0
+        r = len(nums) - 1
         first = -1
 
-        while left <= right:
-            mid = (left + right) // 2
+        while l <= r:
+            mid = (l + r) // 2
 
             if nums[mid] == target:
-                first = mid 
-                right = right - 1
+                r = mid - 1
+                first = mid
             elif nums[mid] < target:
-                left = mid + 1
+                l = mid + 1
             else:
-                right = mid - 1
-        
-        left = 0
-        right = len(nums) - 1
+                r = mid - 1
+
+        l = 0
+        r = len(nums) - 1
         last = -1
 
-        while left <= right:
-            mid = (left + right) // 2
+        while l <= r:
+            mid = (l + r) // 2
 
             if nums[mid] == target:
+                l = mid + 1
                 last = mid 
-                left = mid + 1
             elif nums[mid] < target:
-                left = mid + 1
+                l = mid + 1
             else:
-                right = mid - 1
-        
+                r = mid - 1
         return [first,last]
