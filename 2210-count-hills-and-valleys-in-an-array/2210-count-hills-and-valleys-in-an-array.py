28799f2bdfce5f727edeebba02877f07ebb5e8f9
Suraj Sasmal <surajsasmal9777@gmail.com>
2026-08-15 19:29:58 +0530

    Time: 0 ms (100%), Space: 19.2 MB (87.76%) - LeetHub

--- 2210-count-hills-and-valleys-in-an-array/2210-count-hills-and-valleys-in-an-array.py
+++ 2210-count-hills-and-valleys-in-an-array/2210-count-hills-and-valleys-in-an-array.py
@@ -1,19 +1,35 @@
 class Solution:
     def countHillValley(self, nums: List[int]) -> int:
         
-        nums2 = []
+        # nums2 = []
 
-        for num in nums:
-            if not nums2 or nums2[-1] != num:
-                nums2.append(num)
+        # for num in nums:
+        #     if not nums2 or nums2[-1] != num:
+        #         nums2.append(num)
+
+        # ans = 0
+        # n = len(nums2)
+
+        # for i in range(1,n-1):
+        #     if nums2[i - 1] < nums2[i] > nums2[i + 1]:
+        #         ans += 1
+        #     if nums2[i - 1] > nums2[i] < nums2[i + 1]:
+        #         ans += 1
+        # return ans
 
         ans = 0
-        n = len(nums2)
 
-        for i in range(1,n-1):
-            if nums2[i - 1] < nums2[i] > nums2[i + 1]:
+        prev = nums[0]
+
+        n = len(nums)
+
+        for i in range(1,n - 1):
+            if nums[i] == nums[i + 1]:
+                continue
+            if prev < nums[i] > nums[i + 1]:
                 ans += 1
-            if nums2[i - 1] > nums2[i] < nums2[i + 1]:
+            elif prev > nums[i] < nums[i + 1]:
                 ans += 1
-        return ans
+            prev = nums[i]
+        return ans 
         
