ea80b1b76c837b85db2434d8490a9e2461e8898b
Suraj Sasmal <surajsasmal9777@gmail.com>
2026-08-17 07:04:58 +0530

    Time: 0 ms (100%), Space: 31.3 MB (48.35%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.py
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.py
@@ -6,11 +6,11 @@ class Solution:
         r = len(arr) - 1
 
         while l < r:
-
             mid = (l + r) // 2
-
+            
             if arr[mid] < arr[mid + 1]:
                 l = mid + 1
             else:
                 r = mid 
         return l
+        
