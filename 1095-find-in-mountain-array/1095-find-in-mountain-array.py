0a9eaf5bb1e8f328f56ad17f837e806e78c0afa1
Suraj Sasmal <surajsasmal9777@gmail.com>
2026-08-17 06:34:26 +0530

    Time: 51 ms (21.41%), Space: 19.9 MB (90.29%) - LeetHub

---
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.py
@@ -0,0 +1,52 @@
+# """
+# This is MountainArray's API interface.
+# You should not implement it, or speculate about its implementation
+# """
+#class MountainArray:
+#    def get(self, index: int) -> int:
+#    def length(self) -> int:
+
+class Solution:
+    def findInMountainArray(self, target: int, mountainArr: 'MountainArray') -> int:
+        
+        l = 0
+
+        r = mountainArr.length() - 1 
+
+        while l < r:
+            mid = (l + r) // 2
+
+            if mountainArr.get(mid) < mountainArr.get(mid + 1):
+                l = mid + 1
+            else:
+                r = mid 
+        peak = l
+
+        l = 0
+        r = peak
+
+        while l <= r:
+            mid = (l + r) // 2
+
+            if mountainArr.get(mid) == target:
+                return mid 
+            elif mountainArr.get(mid) < target:
+                l = mid + 1
+            else:
+                r = mid - 1
+
+        l = peak + 1
+        r = mountainArr.length() - 1
+
+        while l <= r:
+            mid = (l + r) // 2
+
+            if mountainArr.get(mid) == target:
+                return mid 
+            elif mountainArr.get(mid) > target:
+                l = mid + 1
+            else:
+                r = mid - 1
+        
+        return -1
+        
