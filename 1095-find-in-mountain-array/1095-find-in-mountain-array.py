05ba6c1790bd78853361e7700d048b6efe88123a
Suraj Sasmal <surajsasmal9777@gmail.com>
2026-08-18 10:19:35 +0530

    Time: 53 ms (12.39%), Space: 19.8 MB (98.55%) - LeetHub

--- 1095-find-in-mountain-array/1095-find-in-mountain-array.py
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.py
@@ -9,44 +9,43 @@
 class Solution:
     def findInMountainArray(self, target: int, mountainArr: 'MountainArray') -> int:
         
+        
         n = mountainArr.length()
         l = 0
-        r = n - 1 
-
+        r = n - 1
+        # finding the peak element 
         while l < r:
             mid = (l + r) // 2
-
             if mountainArr.get(mid) < mountainArr.get(mid + 1):
                 l = mid + 1
             else:
                 r = mid 
+
         peak = l
 
+        # left side search 
         l = 0
         r = peak
 
         while l <= r:
             mid = (l + r) // 2
-
             if mountainArr.get(mid) == target:
-                return mid 
+                return mid
             elif mountainArr.get(mid) < target:
                 l = mid + 1
             else:
                 r = mid - 1
-
-        l = peak + 1
+        
+        # right side search 
+        l = peak 
         r = n - 1
 
         while l <= r:
             mid = (l + r) // 2
-
             if mountainArr.get(mid) == target:
-                return mid 
+                return mid
             elif mountainArr.get(mid) > target:
                 l = mid + 1
             else:
                 r = mid - 1
-        
         return -1
-        
