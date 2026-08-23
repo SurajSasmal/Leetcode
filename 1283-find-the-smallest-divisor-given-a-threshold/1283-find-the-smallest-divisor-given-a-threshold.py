d10d8a2173aa8914030a35857577f2969ac0fa1a
Suraj Sasmal <surajsasmal9777@gmail.com>
2026-08-23 12:49:50 +0530

    Time: 128 ms (64.2%), Space: 24.3 MB (77.11%) - LeetHub

--- 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.py
+++ 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.py
@@ -1,21 +1,20 @@
 class Solution:
     def smallestDivisor(self, nums: List[int], threshold: int) -> int:
         
-        l = 1
-        r = max(nums)
-        # ans = 0
-        while l <= r:
-            mid = (l + r) // 2
+        left = 1
+
+        right = max(nums)
+
+        while left <= right:
+            mid = (left + right) // 2
             total = 0
+
             for num in nums:
                 divisor = (num + mid - 1) // mid 
-                total += divisor 
-
+                total += divisor
+            
             if total <= threshold:
-                # ans = mid
-                r = mid - 1
-            elif total > threshold:
-                l = mid + 1
+                right = mid - 1
             else:
-                r = mid - 1
-        return r + 1
+                left = mid + 1
+        return right + 1
