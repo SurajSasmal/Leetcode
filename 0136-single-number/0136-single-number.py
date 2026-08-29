67cb2b26165422c736378a10115af17e74f9afbd
Suraj Sasmal <surajsasmal9777@gmail.com>
2026-08-29 20:14:18 +0530

    Time: 0 ms (100%), Space: 21.2 MB (29.89%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -2,7 +2,7 @@ class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         
         total = 0
-        for i in range(len(nums)):
-            total = total ^ nums[i]
-        
+        for num in nums:
+            total = total ^ num
         return total 
+        
